a4b5c5bbf45fa1b745b2e1192a6bac0a15dd4d20
mohammadnassem <mohammadnassem333@gmail.com>
2026-08-01 06:29:45 +0300

    update home page

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -1,20 +1,12 @@
-import { Component, inject } from '@angular/core';
-import { AuthService, LocalizationPipe } from '@abp/ng.core';
+import { LocalizationPipe } from '@abp/ng.core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [LocalizationPipe]
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [LocalizationPipe, RouterLink],
 })
-export class HomeComponent {
-  private authService = inject(AuthService);
-
-  get hasLoggedIn(): boolean {
-    return this.authService.isAuthenticated
-  }
-
-  login() {
-    this.authService.navigateToLogin();
-  }
-}
+export class HomeComponent {}
